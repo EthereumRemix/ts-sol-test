7c27aebca36a93b85c8d9ea82652a1307768bd02
ioedeveloper <daviddisu8@gmail.com>
2022-12-27 17:46:47 +0100

    Build new changes

--- dist/index.js
+++ dist/index.js
@@ -282,7 +282,7 @@ function compileContract(contractPath, settings) {
                 case 2:
                     compilerList = _b.sent();
                     console.log('before resolving dependency');
-                    return [4 /*yield*/, axios_1.default.get('https://unpkg.com/@openzeppelin/contracts@4.8.0/proxy/ERC1967/ERC1967Upgrade.sol', { transformResponse: [] })];
+                    return [4 /*yield*/, axios_1.default.get('https://unpkg.com/@openzeppelin/contracts@4.8.0/proxy/ERC1967/ERC1967Upgrade.sol')];
                 case 3:
                     resolveDependecy = _b.sent();
                     console.log('resolveDependecy: ', resolveDependecy);
