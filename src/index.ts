edbacce650daaa0e9bfe282905731d0a2684a2a0
ioedeveloper <daviddisu8@gmail.com>
2023-01-20 17:31:56 +0100

    Use remix-simulator

--- src/index.ts
+++ src/index.ts
@@ -214,12 +214,12 @@ async function setupRunEnv (): Promise<void> {
   const isNPMrepo = existsSync(packageLock)
 
   if (isYarnRepo) {
-    await cli.exec('yarn', ['add', 'mocha', '@remix-project/ghaction-helper', '--dev'])
+    await cli.exec('yarn', ['add', 'mocha', '@remix-project/ghaction-helper@0.1.4-beta.0', '--dev'])
   } else if (isNPMrepo) {
-    await cli.exec('npm', ['install', 'tslib', 'mocha', '@remix-project/ghaction-helper', '--save-dev', '--legacy-peer-deps'])
+    await cli.exec('npm', ['install', 'tslib', 'mocha', '@remix-project/ghaction-helper@0.1.4-beta.0', '--save-dev', '--legacy-peer-deps'])
   } else {
     await cli.exec('npm', ['init', '-y'])
-    await cli.exec('npm', ['install', 'tslib', 'mocha', '@remix-project/ghaction-helper', '--save-dev', '--legacy-peer-deps'])
+    await cli.exec('npm', ['install', 'tslib', 'mocha', '@remix-project/ghaction-helper@0.1.4-beta.0', '--save-dev', '--legacy-peer-deps'])
   }
 }
 
